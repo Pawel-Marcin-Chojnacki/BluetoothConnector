857ff9881243e55d4b5ec2a5e3f07298c1c3c226
Paweł Marcin Chojnacki <pawel.marcin.chojnacki@gmail.com>
2017-07-26 16:51:31 +0200

    Created empty project.

---
+++ app/src/main/java/com/example/pawel/bluetoothconnector/HelloWorldMessage.java
@@ -0,0 +1,13 @@
+package com.example.pawel.bluetoothconnector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloWorldMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world_message);
+    }
+}
